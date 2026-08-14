62a7bc77efce3cf3ecfdd8fd608f8166baec809d
Ming Butler <Minglibutler@gmail.com>
2026-08-14 15:33:57 -0400

    Add mart dags triggers when new data lands(OR semantics)

---
+++ airflow/dags/mart_flights_triggered_by_bts.py
@@ -0,0 +1,21 @@
+from airflow.sdk import dag, task, Asset
+
+bts_raw_asset = Asset("gcs://flight-delay-raw/raw/bts/")
+
+@dag(
+    schedule=[bts_raw_asset], # triggers when new BTS data lands in GCS
+    catchup=False,
+    tags=["mart", "asset-triggered", "bts"]
+)
+def mart_flights_triggered_by_bts():
+    @task.bash
+    def dbt_run_mart():
+        return "cd /opt/dbt_project && dbt run --select stg_bts_ontime+ stg_live_flights+ --target prod"
+    
+    @task.bash
+    def dbt_test_mart():
+        return "cd /opt/dbt_project && dbt test --select stg_bts_ontime+ stg_live_flights+"
+    
+    dbt_run_mart() >> dbt_test_mart() # pyright: ignore [reportUnusedExpression]
+    
+mart_flights_triggered_by_bts()
---
+++ airflow/dags/mart_flights_triggered_by_live.py
@@ -0,0 +1,21 @@
+from airflow.sdk import dag, task, Asset
+
+live_raw_asset = Asset("gcs://flight-delay-raw/raw/live/")
+
+@dag(
+    schedule=[live_raw_asset], # triggers when new live data lands in GCS
+    catchup=False,
+    tags=["mart", "asset-triggered", "live"]
+)
+def mart_flights_triggered_by_live():
+    @task.bash
+    def dbt_run_mart():
+        return "cd /opt/dbt_project && dbt run --select stg_bts_ontime+ stg_live_flights+ --target prod"
+    
+    @task.bash
+    def dbt_test_mart():
+        return "cd /opt/dbt_project && dbt test --select stg_bts_ontime+ stg_live_flights+"
+    
+    dbt_run_mart() >> dbt_test_mart() # pyright: ignore [reportUnusedExpression]
+    
+mart_flights_triggered_by_live()
