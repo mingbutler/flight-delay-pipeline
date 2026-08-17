79576148e03552b3d9ad3d40c98bc52a6449f8fe
Ming Butler <Minglibutler@gmail.com>
2026-08-16 20:15:41 -0400

    Fix: exclude seconds from AeroDataBox 'local' times

--- dbt_project/models/staging/stg_live_flights.sql
+++ dbt_project/models/staging/stg_live_flights.sql
@@ -5,10 +5,12 @@ select
     Origin,
     Dest,
     -- only need time of day from ISO local 
-    time(timestamp(CRSDepTime)) as scheduled_dep_time,
-    time(timestamp(DepTime)) as actual_dep_time,
-    time(timestamp(CRSArrTime)) as scheduled_arr_time,
-    time(timestamp(ArrTime)) as actual_arr_time,
+    -- AeroDataBox 'local' does not include seconds
+    -- safe nulls anything malformed
+    time(safe.parse_timestamp('%Y-%m-%d %H:%M%Ez', CRSDepTime)) as scheduled_dep_time,
+    time(safe.parse_timestamp('%Y-%m-%d %H:%M%Ez', DepTime)) as actual_dep_time,
+    time(safe.parse_timestamp('%Y-%m-%d %H:%M%Ez', CRSArrTime)) as scheduled_arr_time,
+    time(safe.parse_timestamp('%Y-%m-%d %H:%M%Ez', ArrTime)) as actual_arr_time,
     FlightStatus as flight_status,
     Direction as direction
 from {{ source('raw', 'live_flights') }}
